d3a9d2da1154183ec104012a047525d486a7f507
shah 911 <mianshahzeb911@gmail.com>
2024-12-27 23:01:42 +0500

    first commit

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -10,7 +10,7 @@ const Contact = () => {
           src="/sence-1.jpg"
           alt=""
           fill
-          priority
+          priority={true}
           className="object-cover"
         />
         <span className="uppercase text-[10vw] font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
